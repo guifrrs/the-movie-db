0ed85d4267e82f50dce813b30c1bc50ec679ae68
Guilherme Silva <antonioguil@gmail.com>
2023-07-15 18:13:18 -0300

    feat: get current search params to list active filters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+import { FilterList } from '@/components/filter-list/filter-list'
 import { MovieList } from '@/components/movie-list/movie-list'
-import Image from 'next/image'
+import { getActiveFilters } from '@/utils/get-active-filters'
 
-export default function Home() {
+interface HomeProps {
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export default function Home({ searchParams }: HomeProps) {
+  const activeFilters = getActiveFilters(searchParams)
+
   return (
     <main className="min-h-screen bg-white text-black">
-      <MovieList />
+      <section className="flex flex-col md:items-center bg-violet-950 p-4">
+        <h4 className="mb-8 font-bold text-2xl text-white">
+          Milhões de filmes, séries e pessoas para descobrir. Explore já.
+        </h4>
+
+        <p className="uppercase font-bold text-sm text-white">Filtre por:</p>
+
+        <FilterList activeFilters={activeFilters} />
+      </section>
+
+      <section className="p-4">
+        <MovieList activeFilters={activeFilters} />
+      </section>
     </main>
   )
 }
