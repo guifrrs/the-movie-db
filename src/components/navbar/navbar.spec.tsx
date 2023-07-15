52797dc9df23a3b7d9b8d6662ada31300ed481a1
Guilherme Silva <antonioguil@gmail.com>
2023-07-15 15:40:38 -0300

    chore: setup Jest and add first test

---
+++ src/components/navbar/navbar.spec.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import { Navbar } from './navbar'
+
+describe('Navbar', () => {
+  it('render successfuly', () => {
+    render(<Navbar />)
+
+    expect(screen.getByAltText('TMDB logo')).toBeInTheDocument()
+  })
+})
