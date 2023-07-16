089b44dc968a921c3fee1b4b76301bad4c5d5f7a
Guilherme Silva <antonioguil@gmail.com>
2023-07-16 12:18:48 -0300

    fix: trailer size

--- src/components/trailer/trailer.tsx
+++ src/components/trailer/trailer.tsx
@@ -13,8 +13,8 @@ export async function Trailer({ movieId }: TrailerProps) {
   const trailer = getTrailer(trailerData)
 
   return (
-    <div>
-      <iframe className="w-full h-80" title="trailer" src={`https://www.youtube.com/embed/${trailer.key}`}></iframe>
+    <div className="aspect-w-16 aspect-h-9 max-w-7xl sm:h-[30rem]">
+      <iframe className="w-full h-full" title="trailer" src={`https://www.youtube.com/embed/${trailer.key}`}></iframe>
     </div>
   )
 }
