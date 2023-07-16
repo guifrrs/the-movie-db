3453062e2fc8e4eeb74682f69c63d2835c2e485f
Guilherme Silva <antonioguil@gmail.com>
2023-07-16 11:33:53 -0300

    fix: make movie list fetch specific movie if id is present

--- src/components/movie-list/movie-list.tsx
+++ src/components/movie-list/movie-list.tsx
@@ -1,13 +1,21 @@
 import { MovieItem } from '@/components'
-import { Filter } from '@/types'
-import { fetchMovies } from '@/utils'
+import { APIPopularResponse, Filter } from '@/types'
+import { API, fetchData, getMovies } from '@/utils'
 
 interface MovieListProps {
   activeFilters: Filter[]
+  movieId: string | null
 }
 
-export async function MovieList({ activeFilters }: MovieListProps) {
-  const movies = await fetchMovies(activeFilters)
+export async function MovieList({ activeFilters, movieId }: MovieListProps) {
+  const genresFilter = activeFilters.length ? `&with_genres=${activeFilters}` : ''
+  const url = movieId ? `${API.MOVIE}/${movieId}/recommendations` : API.POPULAR.concat(genresFilter)
+
+  const movieData = await fetchData<APIPopularResponse>({
+    url,
+  })
+
+  const movies = getMovies(movieData)
 
   return (
     <ul className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5">
