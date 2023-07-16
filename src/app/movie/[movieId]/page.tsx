e663f5f6cfbf6d0af83d9607c2a1cf16a4eebe2d
Guilherme Silva <antonioguil@gmail.com>
2023-07-16 11:34:28 -0300

    feat: add initial movie details page

--- src/app/movie/[movieId]/page.tsx
+++ src/app/movie/[movieId]/page.tsx
@@ -1,7 +1,39 @@
-export default function MovieDetails() {
+import { CastList, MovieList, Section, Trailer } from '@/components'
+import { MovieDetails } from '@/components/movie-details/movie-details'
+
+export default function Movie({
+  params,
+}: {
+  params: {
+    movieId: string
+  }
+}) {
+  const { movieId } = params
+
   return (
     <main className="min-h-screen bg-white text-black">
-      <h1>movie page</h1>
+      <MovieDetails movieId={movieId} />
+
+      <Section>
+        <Section.Title title="Elenco original" />
+        <Section.Content>
+          <CastList movieId={movieId} />
+        </Section.Content>
+      </Section>
+
+      <Section>
+        <Section.Title title="Trailer" />
+        <Section.Content>
+          <Trailer movieId={movieId} />
+        </Section.Content>
+      </Section>
+
+      <Section>
+        <Section.Title title="Recomendações" />
+        <Section.Content>
+          <MovieList activeFilters={[]} movieId={movieId} />
+        </Section.Content>
+      </Section>
     </main>
   )
 }
